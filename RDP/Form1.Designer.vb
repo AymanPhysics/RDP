0fdc26c818d2320a0bf5e7a38ecb89e32a0cc110
Ayman Physics <mr_aymans@hotmail.com>
2022-11-05 03:59:40 +0200

    0

---
+++ RDP/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.G = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RDP = New AxMSTSCLib.AxMsRdpClient9NotSafeForScripting()
+        Me.ColName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColIP = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColConnect = New System.Windows.Forms.DataGridViewButtonColumn()
+        CType(Me.G, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RDP, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'G
+        '
+        Me.G.AllowUserToAddRows = False
+        Me.G.AllowUserToDeleteRows = False
+        Me.G.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.G.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.G.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.G.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ColName, Me.ColIP, Me.ColConnect})
+        Me.G.Location = New System.Drawing.Point(12, 12)
+        Me.G.Name = "G"
+        Me.G.RowHeadersVisible = False
+        Me.G.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.G.ShowEditingIcon = False
+        Me.G.Size = New System.Drawing.Size(387, 271)
+        Me.G.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(12, 290)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(387, 28)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Full Screen"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RDP
+        '
+        Me.RDP.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RDP.Enabled = True
+        Me.RDP.Location = New System.Drawing.Point(405, 12)
+        Me.RDP.Name = "RDP"
+        Me.RDP.OcxState = CType(resources.GetObject("RDP.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.RDP.Size = New System.Drawing.Size(210, 306)
+        Me.RDP.TabIndex = 0
+        '
+        'ColName
+        '
+        Me.ColName.FillWeight = 300.0!
+        Me.ColName.HeaderText = "Name"
+        Me.ColName.Name = "ColName"
+        '
+        'ColIP
+        '
+        Me.ColIP.FillWeight = 200.0!
+        Me.ColIP.HeaderText = "IP"
+        Me.ColIP.Name = "ColIP"
+        '
+        'ColConnect
+        '
+        Me.ColConnect.FillWeight = 150.0!
+        Me.ColConnect.HeaderText = "Connect"
+        Me.ColConnect.Name = "ColConnect"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(627, 330)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.G)
+        Me.Controls.Add(Me.RDP)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.G, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RDP, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents RDP As AxMSTSCLib.AxMsRdpClient9NotSafeForScripting
+    Friend WithEvents G As System.Windows.Forms.DataGridView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ColName As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents ColIP As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents ColConnect As System.Windows.Forms.DataGridViewButtonColumn
+
+End Class
---
+++ RDP/Form1.vb
@@ -0,0 +1,97 @@
+﻿Imports System.IO
+Imports MSTSCLib
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GetIPs()
+        WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub GetIPs()
+        Dim sr As New StreamReader(Application.StartupPath & "\IPs.txt")
+        Try
+            While True
+                Dim L As String = sr.ReadLine
+                G.Rows.Add({L.Split(":")(0), L.Split(":")(1), "Connect"})
+            End While
+        Catch ex As Exception
+        End Try
+        G.Sort(G.Columns(0), System.ComponentModel.ListSortDirection.Ascending)
+        G.CurrentCell = G.Rows(0).Cells(2)
+        sr.Close()
+        G.AllowUserToResizeColumns = False
+        G.AllowUserToResizeRows = False
+    End Sub
+
+    Dim CurrentCell As DataGridViewCell
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles G.CellClick
+        If G.CurrentRow Is Nothing Then Return
+        If RDP.Connected <> 0 Then
+            RDP.Disconnect()
+            If CurrentCell Is G.CurrentRow.Cells(2) Then Return
+        End If
+        Try
+            If Not CurrentCell Is Nothing Then CurrentCell.Value = "Connect"
+            CurrentCell = G.CurrentRow.Cells(2)
+            CurrentCell.Value = "Connecting..."
+            RDP.Server = G.CurrentRow.Cells(1).Value
+            RDP.UserName = "Physics"
+            Dim secured As IMsTscNonScriptable = RDP.GetOcx()
+            secured.ClearTextPassword = "Phy123!@#"
+            
+            RDP.AdvancedSettings9.RedirectClipboard = True
+            RDP.AdvancedSettings9.RedirectDrives = True
+            'RDP.AdvancedSettings9.WorkDir = "F:\"
+
+            RDP.AdvancedSettings9.AuthenticationLevel = 0
+            RDP.AdvancedSettings9.EnableCredSspSupport = True
+            RDP.DesktopWidth = SystemInformation.VirtualScreen.Width
+            RDP.DesktopHeight = SystemInformation.VirtualScreen.Height
+            RDP.FullScreen = IsFullScreen
+            RDP.Connect()
+            Text = RDP.Server & " : " & RDP.ConnectingText
+        Catch
+        End Try
+    End Sub
+
+    Private Sub RDP_OnConnected(sender As Object, e As EventArgs) Handles RDP.OnConnected
+        CurrentCell.Value = "Disconnect"
+        Text = RDP.Server & " : " & RDP.ConnectingText
+    End Sub
+
+    Private Sub AxMsRdpClient9NotSafeForScripting1_OnDisconnected(sender As Object, e As AxMSTSCLib.IMsTscAxEvents_OnDisconnectedEvent) Handles RDP.OnDisconnected
+        CurrentCell.Value = "Connect"
+        Text = RDP.Server & " : " & RDP.ConnectingText
+    End Sub
+
+    Dim IsFullScreen As Boolean = True
+    Dim RDP_Width As Double = 0
+    Dim RDP_Height As Double = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        IsFullScreen = Not IsFullScreen
+        RDP.FullScreen = IsFullScreen
+    End Sub
+
+    Private Sub RDP_OnEnterFullScreenMode(sender As Object, e As EventArgs) Handles RDP.OnEnterFullScreenMode
+        Try
+            IsFullScreen = True
+            RDP_Width = RDP.DesktopWidth
+            RDP_Height = RDP.DesktopHeight
+            RDP.DesktopWidth = SystemInformation.VirtualScreen.Width
+            RDP.DesktopHeight = SystemInformation.VirtualScreen.Height
+        Catch ex As Exception
+            RDP.Reconnect(SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height)
+        End Try
+    End Sub
+
+    Private Sub RDP_OnLeaveFullScreenMode(sender As Object, e As EventArgs) Handles RDP.OnLeaveFullScreenMode
+        Try
+            IsFullScreen = False
+            RDP.DesktopWidth = RDP.Width
+            RDP.DesktopHeight = RDP.Height
+        Catch ex As Exception
+            RDP.Reconnect(RDP.Width, RDP.Height)
+        End Try
+    End Sub
+End Class
